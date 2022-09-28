99a1841e14cc77bcdeaeab1190b43e35a15becb3
James Chien <shc261392@gmail.com>
2022-09-28 18:03:34 +0800

    build: use rollup and api-extractor for bundling

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+import json from "@rollup/plugin-json";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: "src/index.ts",
+    output: {
+      name: "estuary-upload",
+      file: pkg.browser,
+      format: "umd",
+      sourcemap: false,
+    },
+    plugins: [
+      json(),
+      resolve(), //
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true},
+      { file: pkg.module, format: "es", sourcemap: true},
+    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+  },
+];
